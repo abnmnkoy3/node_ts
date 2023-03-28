29758abfe7266286307f963bfc69bcf630a6a38b
Jakkawan <abnmnkoy4@gmail.com>
2023-03-28 16:36:39 +0700

    2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { json } from 'body-parser';
 import express, { Application, Request, Response } from 'express';
+import { Query } from "express-serve-static-core";
+import { User } from "./model/user.type";
 var config = require('../vandapa1_kpi.json');
 var mysql = require('mysql');
 // const mysql = require('mysql2');
@@ -12,33 +14,26 @@ const con = mysql.createConnection({
     database: 'vandapa1_kpi',
     port: '3306'
 });
-
 server.use(express.json());
-// con.connect((error) => {
-//     if (err) {
-//         console.log('Error connecting to MySQL database =', err);
-//         return;
-//     }
-//     console.log('MySQL successfully connectted')
-// })
+export interface TypeRequestQuery<T extends Query> extends Express.Request {
+    query: T;
+}
+server.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const rows = con.query(
+            `SELECT * FROM employee WHERE Em_Email = ?`,
+            [email]
 
-server.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    // try {
-    const rows = con.query(
-        `SELECT * FROM employee WHERE Em_Email = ?`,
-        [email]
+        )
+        res.status(201).json(rows);
+    } catch (e) {
 
-    )
-     console.log(json(rows));
+    }
 });
-// } catch (err) {
-
-// }
-// })
 
 
-server.get('/api', (req: Request, res: Response) => res.json({ message: 'Express + TypeScript Server Test' }));
+server.get('/api', (req: TypeRequestQuery<User>, res: Response) => res.json(req.query.username));
 
 server.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
